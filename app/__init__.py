bb8b886c80dc175883dbe9e74e4534ab18c2b886
Matvej Melikhov <matvej.melikhov@yandex.ru>
2024-10-25 12:38:44 +0300

    fix babel double init

--- app/__init__.py
+++ app/__init__.py
@@ -22,8 +22,7 @@ def get_locale():
         return lang
     return request.accept_languages.best_match(application.config['BABEL_SUPPORTED_LOCALES'])
 
-babel = Babel(application)
-babel.init_app(application, locale_selector=get_locale)
+babel = Babel(application, locale_selector=get_locale)
 
 
 
